fd4ac55f22e1c08c5e546b32363f5e8be1c8c043
Diwan Malla <diwanmalla@192-168-1-3.tpgi.com.au>
2024-06-23 22:30:52 +1000

    Added footer section

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -1,5 +1,134 @@
+import {
+  MDBFooter,
+  MDBContainer,
+  MDBIcon,
+  MDBInput,
+  MDBCol,
+  MDBRow,
+  MDBBtn,
+} from "mdb-react-ui-kit";
+import {
+  FaFacebookF,
+  FaGithub,
+  FaGoogle,
+  FaInstagram,
+  FaLinkedin,
+  FaSquareTwitter,
+  FaTwitter,
+} from "react-icons/fa6";
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div>
+      <MDBFooter className="text-center" color="white" bgColor="dark">
+        <MDBContainer className="p-4">
+          <section className="mb-4">
+            <MDBBtn
+              outline
+              color="light"
+              floating
+              className="m-1"
+              href="#!"
+              role="button"
+            >
+              <FaFacebookF />
+            </MDBBtn>
+
+            <MDBBtn
+              outline
+              color="light"
+              floating
+              className="m-1"
+              href="#!"
+              role="button"
+            >
+              <FaTwitter />
+            </MDBBtn>
+
+            <MDBBtn
+              outline
+              color="light"
+              floating
+              className="m-1"
+              href="#!"
+              role="button"
+            >
+              <FaGoogle />
+            </MDBBtn>
+
+            <MDBBtn
+              outline
+              color="light"
+              floating
+              className="m-1"
+              href="#!"
+              role="button"
+            >
+              <FaInstagram />
+            </MDBBtn>
+
+            <MDBBtn
+              outline
+              color="light"
+              floating
+              className="m-1"
+              href="#!"
+              role="button"
+            >
+              <FaLinkedin />
+            </MDBBtn>
+
+            <MDBBtn
+              outline
+              color="light"
+              floating
+              className="m-1"
+              href="#!"
+              role="button"
+            >
+              <FaGithub />
+            </MDBBtn>
+          </section>
+
+          <section className="">
+            <form action="">
+              <MDBRow className="d-flex justify-content-center">
+                <MDBCol size="auto">
+                  <p className="pt-2">
+                    <strong>Sign up for our newsletter</strong>
+                  </p>
+                </MDBCol>
+
+                <MDBCol md="5" start>
+                  <MDBInput
+                    contrast
+                    type="email"
+                    label="Email address"
+                    className="mb-4"
+                  />
+                </MDBCol>
+
+                <MDBCol size="auto">
+                  <MDBBtn outline color="light" type="submit" className="mb-4">
+                    Subscribe
+                  </MDBBtn>
+                </MDBCol>
+              </MDBRow>
+            </form>
+          </section>
+        </MDBContainer>
+
+        <div
+          className="text-center p-3"
+          style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
+        >
+          © 2024 Copyright:
+          <a className="text-white" href="https://mdbootstrap.com/">
+            Diwan Malla
+          </a>
+        </div>
+      </MDBFooter>
+    </div>
+  );
 };
 
 export default Footer;
